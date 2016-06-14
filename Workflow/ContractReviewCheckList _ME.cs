dff0181ffeb1e144174882d36be237bb23aa5b82
Christian Bracamontes <cbracamontes@ati.local>
2016-06-14 15:16:39 -0700

    Same functionality just shortened code

--- Workflow/ContractReviewCheckList _ME.cs
+++ Workflow/ContractReviewCheckList _ME.cs
@@ -19,549 +19,199 @@ namespace Workflow
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox8.Visible == false)
-            {
-                textBox8.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox8.Visible = false;
-            }
+            textBox8.Visible = !textBox8.Visible;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (textBox9.Visible == false)
-            {
-                textBox9.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox9.Visible = false;
-            }
+            textBox9.Visible = !textBox9.Visible;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (textBox10.Visible == false)
-            {
-                textBox10.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox10.Visible = false;
-            }
+            textBox10.Visible = !textBox10.Visible;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (textBox11.Visible == false)
-            {
-                textBox11.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox11.Visible = false;
-            }
+            textBox11.Visible = !textBox11.Visible;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (textBox12.Visible == false)
-            {
-                textBox12.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox12.Visible = false;
-            }
+            textBox12.Visible = !textBox12.Visible;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (textBox13.Visible == false)
-            {
-                textBox13.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox13.Visible = false;
-            }
+            textBox13.Visible = !textBox13.Visible;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (textBox14.Visible == false)
-            {
-                textBox14.Visible = true;
-                
-            }
-            else
-            {
-               
-                textBox14.Visible = false;
-            }
+            textBox14.Visible = !textBox14.Visible;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (textBox15.Visible == false)
-            {
-                textBox15.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox15.Visible = false;
-            }
+            textBox15.Visible = !textBox15.Visible;
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            if (textBox16.Visible == false)
-            {
-                textBox16.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox16.Visible = false;
-            }
+            textBox16.Visible = !textBox16.Visible;
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            if (textBox17.Visible == false)
-            {
-                textBox17.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox17.Visible = false;
-            }
+            textBox17.Visible = !textBox17.Visible;
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            if (textBox18.Visible == false)
-            {
-                textBox18.Visible = true;
-               
-            }
-            else
-            {
-               
-                textBox18.Visible = false;
-            }
+            textBox18.Visible = !textBox18.Visible;
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            if (textBox19.Visible == false)
-            {
-                textBox19.Visible = true;
-                
-            }
-            else
-            {
-               
-                textBox19.Visible = false;
-            }
+            textBox19.Visible = !textBox19.Visible;
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            if (textBox20.Visible == false)
-            {
-                textBox20.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox20.Visible = false;
-            }
+            textBox20.Visible = !textBox20.Visible;
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            if (textBox21.Visible == false)
-            {
-                textBox21.Visible = true;
-                
-            }
-            else
-            {
-               
-                textBox21.Visible = false;
-            }
+            textBox21.Visible = !textBox21.Visible;
         }
 
         private void button16_Click(object sender, EventArgs e)
         {
-            if (textBox22.Visible == false)
-            {
-                textBox22.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox22.Visible = false;
-            }
+            textBox22.Visible = !textBox22.Visible;
         }
 
         private void button17_Click(object sender, EventArgs e)
         {
-            if (textBox23.Visible == false)
-            {
-                textBox23.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox23.Visible = false;
-            }
+            textBox23.Visible = !textBox23.Visible;
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
-            if (textBox24.Visible == false)
-            {
-                textBox24.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox24.Visible = false;
-            }
+            textBox24.Visible = !textBox24.Visible;
         }
 
         private void button19_Click(object sender, EventArgs e)
         {
-            if (textBox25.Visible == false)
-            {
-                textBox25.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox25.Visible = false;
-            }
+            textBox25.Visible = !textBox25.Visible;
         }
 
         private void button20_Click(object sender, EventArgs e)
         {
-            if (textBox26.Visible == false)
-            {
-                textBox26.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox26.Visible = false;
-            }
+            textBox26.Visible = !textBox26.Visible;
         }
 
         private void button21_Click(object sender, EventArgs e)
         {
-            if (textBox27.Visible == false)
-            {
-                textBox27.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox27.Visible = false;
-            }
+            textBox27.Visible = !textBox27.Visible;
         }
 
         private void button22_Click(object sender, EventArgs e)
         {
-            if (textBox28.Visible == false)
-            {
-                textBox28.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox28.Visible = false;
-            }
+            textBox28.Visible = !textBox28.Visible;
         }
 
         private void button23_Click(object sender, EventArgs e)
         {
-            if (textBox29.Visible == false)
-            {
-                textBox29.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox29.Visible = false;
-            }
+            textBox29.Visible = !textBox29.Visible;
         }
 
         private void button24_Click(object sender, EventArgs e)
         {
-            if (textBox30.Visible == false)
-            {
-                textBox30.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox30.Visible = false;
-            }
+            textBox30.Visible = !textBox30.Visible;
         }
 
         private void button25_Click(object sender, EventArgs e)
         {
-            if (textBox31.Visible == false)
-            {
-                textBox31.Visible = true;
-                
-            }
-            else
-            {
-               
-                textBox31.Visible = false;
-            }
+            textBox31.Visible = !textBox31.Visible;
         }
 
         private void button26_Click(object sender, EventArgs e)
         {
-            if (textBox32.Visible == false)
-            {
-                textBox32.Visible = true;
-               
-            }
-            else
-            {
-               
-                textBox32.Visible = false;
-            }
+            textBox32.Visible = !textBox32.Visible;
         }
 
         private void button27_Click(object sender, EventArgs e)
         {
-            if (textBox33.Visible == false)
-            {
-                textBox33.Visible = true;
-              
-            }
-            else
-            {
-                
-                textBox33.Visible = false;
-            }
+            textBox33.Visible = !textBox33.Visible;
         }
 
         private void button28_Click(object sender, EventArgs e)
         {
-            if (textBox34.Visible == false)
-            {
-                textBox34.Visible = true;
-              
-            }
-            else
-            {
-              
-                textBox34.Visible = false;
-            }
+            textBox34.Visible = !textBox34.Visible;
         }
 
         private void button29_Click(object sender, EventArgs e)
         {
-            if (textBox35.Visible == false)
-            {
-                textBox35.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox35.Visible = false;
-            }
+            textBox35.Visible = !textBox35.Visible;
         }
 
         private void button30_Click(object sender, EventArgs e)
         {
-            if (textBox36.Visible == false)
-            {
-                textBox36.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox36.Visible = false;
-            }
+            textBox36.Visible = !textBox36.Visible;
         }
 
         private void button31_Click(object sender, EventArgs e)
         {
-            if (textBox37.Visible == false)
-            {
-                textBox37.Visible = true;
-               
-            }
-            else
-            {
-               
-                textBox37.Visible = false;
-            }
+            textBox37.Visible = !textBox37.Visible;
         }
 
         private void button32_Click(object sender, EventArgs e)
         {
-            if (textBox38.Visible == false)
-            {
-                textBox38.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox38.Visible = false;
-            }
+            textBox38.Visible = !textBox38.Visible;
         }
 
         private void button33_Click(object sender, EventArgs e)
         {
-            if (textBox39.Visible == false)
-            {
-                textBox39.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox39.Visible = false;
-            }
+            textBox39.Visible = !textBox39.Visible;
         }
 
         private void button34_Click(object sender, EventArgs e)
         {
-            if (textBox40.Visible == false)
-            {
-                textBox40.Visible = true;
-                
-            }
-            else
-            {
-               
-                textBox40.Visible = false;
-            }
+            textBox40.Visible = !textBox40.Visible;
         }
 
         private void button35_Click(object sender, EventArgs e)
         {
-            if (textBox41.Visible == false)
-            {
-                textBox41.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox41.Visible = false;
-            }
+            textBox41.Visible = !textBox41.Visible;
         }
 
         private void button36_Click(object sender, EventArgs e)
         {
-            if (textBox42.Visible == false)
-            {
-                textBox42.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox42.Visible = false;
-            }
+            textBox42.Visible = !textBox42.Visible;
         }
 
         private void button37_Click(object sender, EventArgs e)
         {
-            if (textBox43.Visible == false)
-            {
-                textBox43.Visible = true;
-               
-            }
-            else
-            {
-               
-                textBox43.Visible = false;
-            }
+            textBox43.Visible = !textBox43.Visible;
         }
 
         private void button38_Click(object sender, EventArgs e)
         {
-            if (textBox44.Visible == false)
-            {
-                textBox44.Visible = true;
-               
-            }
-            else
-            {
-                
-                textBox44.Visible = false;
-            }
+            textBox44.Visible = !textBox44.Visible;
         }
 
         private void button39_Click(object sender, EventArgs e)
         {
-            if (textBox45.Visible == false)
-            {
-                textBox45.Visible = true;
-              
-            }
-            else
-            {
-                
-                textBox45.Visible = false;
-            }
+            textBox45.Visible = !textBox45.Visible;
         }
 
         private void button40_Click(object sender, EventArgs e)
         {
-            if (textBox46.Visible == false)
-            {
-                textBox46.Visible = true;
-                
-            }
-            else
-            {
-                
-                textBox46.Visible = false;
-            }
+            textBox46.Visible = !textBox46.Visible;
         }
+
 
         private void textBox8_TextChanged(object sender, EventArgs e)
         {
