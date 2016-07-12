feccd19901ce12652e84f6873735395c316d9c72
Daan Leiva <daan.leiva@gmail.com>
2016-07-11 23:34:05 -0700

    Added value to Globals folder

--- Workflow/Globals.cs
+++ Workflow/Globals.cs
@@ -10,5 +10,7 @@ namespace Workflow
     {
         public static readonly string odbc_connection_string = "DSN=jobboss32;UID=jbread;PWD=Cloudy2Day";
         public static readonly string binding_connection_string = "Server=ATI-SQL;Database=uniPoint_Live;UID=jbread;PWD=Cloudy2Day";
+
+        public int value = 10;
     }
 }
